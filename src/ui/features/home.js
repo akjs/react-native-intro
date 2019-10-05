3078e73dec0f0fd05203afa882cb000693c8c1ee
Kevin Isom <kevin.isom@gmail.com>
2019-10-05 22:17:34 +1300

    Home page

--- src/ui/features/home.js
+++ src/ui/features/home.js
@@ -1,2 +1,16 @@
-import React from "react"
-export default () => <main><h1>My App</h1></main>
+import React from 'react'
+import { navigate } from '@reach/router'
+export default () => (
+  <main>
+    <h1>Slide Deck App</h1>
+    <a
+      href="/1"
+      onClick={e => {
+        e.preventDefault()
+        navigate('/1', { state: { move: 'forward' } })
+      }}
+    >
+      Slide 1
+    </a>
+  </main>
+)
