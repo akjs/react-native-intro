597f6c9d8069b40b6c13c384fd45ec225d3a819e
Kevin Isom <kevin.isom@gmail.com>
2019-10-05 22:16:06 +1300

    Code block

---
+++ src/ui/components/code-block.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import nightOwl from 'prism-react-renderer/themes/nightOwl'
+export default ({ children }) => {
+  return (
+    <Highlight
+      {...defaultProps}
+      code={children}
+      language="javascript"
+      theme={nightOwl}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style, padding: '20px' }}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  )
+}
